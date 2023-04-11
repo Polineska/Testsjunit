582c47c7a2688ad81b70235d50df49c6e2fe0f32
polinabog475@gmail.com <polinabog475@gmail.com>
2023-04-11 20:08:32 +0300

    f

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "абвгабвг";
+        String str2 = "аб";
+
+        int count = countMatches (str1, str2);
+        System.out.println(count);
+    }
+
+    public static int countMatches (String str1, String str2){
+        if (str2.isEmpty())
+            throw new RuntimeException();
+        return str1.split(str2, -1).length - 1;
+    }
 }
--- src/Testjunit.java
+++ src/Testjunit.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Testjunit {
+import org.testng.annotations.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class Testjunit {
+    @Test
+    @DisplayName("Вторая строка ни разу не входит в первую")
+    public void  thereisnocountMatchesOfLine2Into1 (){
+        String a="абвгд";
+        String b="лк";
+        assertEquals(0, Main.countMatches(a,b), "The test failed");
+
+    }
+
+    @Test
+    @DisplayName("Вторая строка входит в первую один раз")
+    public void  line1EqualsLine2 (){
+        String a="аб";
+        String b="аб";
+        assertEquals(1, Main.countMatches(a,b), "The test failed");
+
+    }
+
+    @Test
+    @DisplayName("Вторая строка входит в первую несколько раз")
+    public void line2OccursInLine1SeveralTimes() {
+        String a = "абабабаб";
+        String b = "аб";
+        assertEquals(4, Main.countMatches(a,b), "The test failed");
+
+    }
+
+    @Test
+    @DisplayName("Первая строка пустая")
+    public void line1IsEmpty() {
+        String a = "";
+        String b = "аб";
+        assertEquals(0, Main.countMatches(a, b), "The test failed");
+    }
+
+    @Test
+    @DisplayName("Введена пустая вторая строка")
+    public void line2IsEmpty (){
+        assertThrows(Exception.class, ()->{
+                    Main.countMatches("абсд","");
+                }
+        );}
 }
+
+
